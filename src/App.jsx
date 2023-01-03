ac6c93d20699fefef26bd8a7db1fd5c529f16b56
Gabriel Augusto <Gabriel.afg182@gmail.com>
2023-01-03 17:39:31 -0300

    att1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 
 /*
 Consuma a API e liste todos os pokemons da consulta do seguinte endpoint. 
@@ -20,13 +19,32 @@ experiência => base_experience
 EXTRA: se puder ordene por nome.
 */
 
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function App() {
-  const [count, setCount] = useState(0);
+
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://pokeapi.co/api/v2/pokemon')
+      .then((response) => setList(response.data.results));
+    
+    //  axios
+    //    .get("https://pokeapi.co/api/v2/pokemon/6")
+    //    .then(response => console.log(response));
+  }, []);
+
 
   return (
     <>
       <h3>desafio fernandev</h3>
       <h1>consumir api pokémon</h1>
+      <hr />
+      {list.map((item) => {
+        <>{JSON.stringify(item)}</>
+      })}
     </>
   );
 }
