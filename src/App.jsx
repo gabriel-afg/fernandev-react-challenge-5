7973adf0b2cfea0653d8f4a57a49cfbe40249cce
Gabriel Augusto <Gabriel.afg182@gmail.com>
2023-05-12 13:28:18 -0300

    Quase finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -26,16 +26,24 @@ function App() {
 
   const [list, setList] = useState([]);
 
-  useEffect(() => {
+  const fetchListData = () => {
     axios
       .get('https://pokeapi.co/api/v2/pokemon')
-      .then((response) => setList(response.data.results));
-    
-    //  axios
-    //    .get("https://pokeapi.co/api/v2/pokemon/6")
-    //    .then(response => console.log(response));
-  }, []);
+      .then((response) => {
+        const sortedArray = [...response.data.results];
 
+        sortedArray.sort((a,b) => {
+          return a.name.localeCompare(b.name);
+        })
+
+        console.log(sortedArray);
+        setList(sortedArray)
+      });
+  }
+
+  useEffect(() => {
+    fetchListData();
+  }, []);
 
   return (
     <>
@@ -43,10 +51,33 @@ function App() {
       <h1>consumir api pokémon</h1>
       <hr />
       {list.map((item) => {
-        <>{JSON.stringify(item)}</>
+        return <Pokemon key={item.name} data={item} />
       })}
     </>
   );
+}
+
+const Pokemon = ({ data }) => {
+  const [details, setDetails] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(data.url)
+      .then((response) => setDetails(response.data));
+  }, []);
+
+  if (details === null) {
+    return <><div>Carregando...</div></>
+  }
+
+  return <div style={{display: 'flex', alignItems:'center'}}>
+    <img
+      src={details.sprites.front_default}
+      style={{width:40, marginLeft:20}} />
+    <span>
+      <b>{details.name}</b>- EXP {details.base_experience}
+    </span>
+  </div>;
 }
 
 export default App;
